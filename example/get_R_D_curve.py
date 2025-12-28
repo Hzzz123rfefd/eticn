71293a43af67fca334345de8dcb811a9b79dd90a
bijiben <bijiben@example.com>
2025-12-28 12:39:33 +0800

    体教修改之前暂存

--- example/get_R_D_curve.py
+++ example/get_R_D_curve.py
@@ -16,15 +16,24 @@ def R_D_SSIM(bpp_lists, msssim_lists, models):
         'ytick.labelsize': '12',
         'legend.fontsize': '12'
     })
+    lines_dict = {}
+    
     for bpp, psnr, model in zip(bpp_lists, msssim_lists, models):
         if model == "BPG" or model == "JEPG2000" or model == "VVC" or model == "[BPG]" or model == "[JEPG2000]" or model == "[VVC]" or model == "[Baseline]":
-            plt.plot(bpp, psnr, marker='o', label=model, linestyle='--')
+            line, =  plt.plot(bpp, psnr, marker='o', label=model, linestyle='--')
         else:
-            plt.plot(bpp, psnr, marker='o', label=model) 
+            line, =  plt.plot(bpp, psnr, marker='o', label=model) 
+        lines_dict[model] = line
+        
     plt.xlabel('bpp')
     plt.ylabel('MS-SSIM(db)')
     plt.legend(loc='lower right')
     plt.grid(True)
+
+    fixed_order = ['Baseline [Qian (ICLR2022)]', '[Proposed Methed]', '[Tong (NIPS2023)]', '[VVC]', '[BPG]', '[JEPG2000]']
+    ordered_handles = [lines_dict[lab] for lab in fixed_order if lab in lines_dict]
+    plt.legend(ordered_handles, [lab for lab in fixed_order if lab in lines_dict], loc='lower right')
+
     plt.savefig(args.dir_path + "/bpp_ssim.png", dpi = 700)
     plt.show()
 
@@ -38,15 +47,24 @@ def R_D_PSNR(bpp_lists, psnr_lists, models):
         'ytick.labelsize': '12',
         'legend.fontsize': '12'
     })
+    lines_dict = {}
+    
     for bpp, psnr, model in zip(bpp_lists, psnr_lists, models):
         if model == "BPG" or model == "JEPG2000" or model == "VVC"or model == "[BPG]" or model == "[JEPG2000]" or model == "[VVC]" or model == "[Baseline]":
-            plt.plot(bpp, psnr, marker='o', label=model, linestyle='--')
+            line, =  plt.plot(bpp, psnr, marker='o', label=model, linestyle='--')
         else:
-            plt.plot(bpp, psnr, marker='o', label=model)
+            line, =  plt.plot(bpp, psnr, marker='o', label=model)
+        lines_dict[model] = line
+            
     plt.xlabel('bpp')
     plt.ylabel('PSNR(db)')
     plt.legend(loc='lower right')
     plt.grid(True)
+    
+    fixed_order = ['Baseline [Qian (ICLR2022)]', '[Proposed Methed]', '[Tong (NIPS2023)]', '[VVC]', '[BPG]', '[JEPG2000]']
+    ordered_handles = [lines_dict[lab] for lab in fixed_order if lab in lines_dict]
+    plt.legend(ordered_handles, [lab for lab in fixed_order if lab in lines_dict], loc='lower right')
+    
     plt.savefig(args.dir_path + "/bpp_psnr.png", dpi = 700)
     plt.show()
 
@@ -76,6 +94,6 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir_path',type=str, default="result/2-fig5/vic")
+    parser.add_argument('--dir_path',type=str, default="result/2-fig5/gric")
     args = parser.parse_args()
     main(args)
---
+++ example/get_complex.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+import torch
+sys.path.append(os.getcwd())
+import argparse
+from compressai import models
+from compressai.utils import load_config
+import torchprofile
+
+
+
+def main(args):
+    config = load_config(args.model_config_path)
+    """ get model"""
+    net = models[config["model_type"]](**config["model"]).to(config["model"]["device"])
+    
+    total_params = sum(p.numel() for p in net.parameters()) / 1000000
+    print(f"Total parameters: {total_params:,}M")
+    
+    inputs = {
+        "image": torch.randn(1, 3, 512, 512),  # 第一个输入
+    }
+    flops = torchprofile.profile_macs(net, inputs)
+    print("FLOPs:", flops / 1000000000, "G")
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_config_path", type=str, default = "config/camvid/vic.yml")
+    args = parser.parse_args()
+    main(args)
